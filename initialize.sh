0a4d184a0ec9dd600ff068c5e512bacbb7095457
SELO LEE <selo@SELOui-MacBook-Pro.local>
2017-05-05 04:14:34 +0900

    Add configuration for bash_it theme

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-
 echo "== Start initialize =="
+
 
 ## Install Homebrew
 echo "== Install homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "== Installed homebrew"
+## http://searchbrew.com/
 which brew
 brew update
 
@@ -19,12 +20,6 @@ brew cask install duet
 brew cask install iterm2
 brew cask install pycharm
 brew cask install sourcetree
-
-
-## http://searchbrew.com/
-## Install develop env
-brew install node
-brew install pyenv
 
 ## Install Ultimate vimrc (https://github.com/amix/vimrc)
 echo "== Install Ultimate vimrc =="
@@ -45,3 +40,24 @@ git clone --depth=1 https://github.com/SELO77/bash-it.git ~/.bash_it
 ## Copy git config files to Home
 cp git_setting/.gitignore ~/
 cp git_setting/.gitconfig ~/
+
+## Install Node
+brew install node
+
+
+# Install Python
+brew install pyenv
+brew install pyenv-virtualenv
+
+echo '\n\n## Set Pyenv run environment'
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+
+
+# Overide bash_it theme extended by Sexy theme
+echo 'export BASH_IT_THEME="Sexy"' >> ~/.bash_profile
+
+
+# echo 'echo Hi \033[1;31m\]"$(whoami)"\033[0m\], Enjoy' >> ~/.bash_profile
